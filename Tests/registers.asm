47e6ab2ec0f225983ef0042e9eb337d6e9f049b4
13xforever <ilya.veselov@gmail.com>
2012-08-11 18:52:01 +0600

    Registers and fix for invalid fucomipp

---
+++ Tests/registers.asm
@@ -0,0 +1,49 @@
+;invalid
+r0l, r1l, r7l, r16l, r18l, r20l, r21l
+r0w, r1w, r7w, r16w, r18w, r20w, r21w
+r0d, r1d, r7d, r16d, r18d, r20d, r21d
+r0, r1, r7, r16, r18, r20, r21
+xmm16, xmm20, xmm21
+dr4, dr5, dr8, dr9
+cr1, cr5
+st8, st9, st10, st11
+flags
+
+;general purpose
+cl, dl, ah, bh, ch, dh
+al, bl, cl, dl, dil, sil, bpl, spl, r8l, r9l, r10l, r11l, r12l, r13l, r14l, r15l
+ax, dx, di, si, bp, sp
+ax, bx, cx, dx, di, si, bp, sp, r8w, r9w, r10w, r11w, r12w, r13w, r14w, r15w
+eax, ebx, ecx, edx, edi, esi, ebp, esp, r8d, r9d, r10d, r11d, r12d, r13d, r14d, r15d
+rax, rbx, rcx, rdx, rdi, rsi, rbp, rsp, r8, r9, r10, r11, r12, r13, r14, r15
+xmm8, xmm9, xmm10, xmm11, xmm12, xmm13, xmm14, xmm15
+
+;segment
+cs, ds, ss, es, fs, gs
+
+;eflags
+eflags, rflags
+
+;instruction pointer
+eip, rip
+
+;x87 FPU registers
+st0, st1, st2, st3, st4, st5, st6, st7
+;status word, control word, tag word, data operand pointer, and instruction pointer
+
+;MMX registers
+mm0, mm1, mm2, mm3, mm4, mm5, mm6, mm7
+
+;XMM registers
+xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7, mxcsr
+
+;control registers
+cr0, cr2, cr3, cr4
+
+;system table pointer registers 
+gdtr, ldtr, idtr ;and task register
+
+;debug registers
+dr0, dr1, dr2, dr3, dr6, dr7
+
+;msr registers
---
+++ Tests/test.asm
@@ -0,0 +1,67 @@
+;registers
+ip|rip|rax|rbx|rcx|rdx|rdi|rsi|rbp|rsp|
+r8|r9|r10|r11|r12|r13|r14|r15|r8b|r9b|r10b|r11b|r12b|r13b|r14b|r15b|r8w|r9w|r10w|r11w|r12w|r13w|r14w|r15w|r8d|r9d|r10d|r11d|r12d|r13d|r14d|r15d|
+eip|eax|ebx|ecx|edx|edi|esi|ebp|esp|ax|bx|cx|dx|di|si|bp|sp|ah|al|bh|bl|ch|cl|dh|dl|ax|bx|cx|dx|cs|ds|ss|es|fs|gs|cr0|cr2|cr3|db0|db1|db2|db3|db6|db7|tr6|tr7|st
+
+;should be invalid
+fiaddp
+cmovnpo/cmovpos
+fucomipp
+fidivp/fidivrp
+fimulp
+fisubp/fisubrp
+
+;valid
+vaddpd xmm10, [rax+r15*4]
+vaddps ymm1, ymm2, yword [rax]
+andpd ymm1, ymm2
+cmovge
+cmovnz
+cmovz
+cmovo
+cmovpo
+cmpxchg|cmpxchg8b|cmpxchg16b
+cpuid
+cbw/cwde/cdq
+cmps/cmpsb/cmpsw/cmpsd/cmpsq
+cwd/cdq/cqo
+dppd
+emms
+fadd/faddp/fiadd
+fcmovnbe
+fcom/fcomp/fcompp
+fcomi/fcomip/fucomi/fucomip
+fucom/fucomp/fucompp
+ficom/ficomp
+fdiv/fdivp/fidiv
+fdivr/fdivrp/fidivr
+ffree
+fld
+fld1/fldl2t/fldl2e/fldpi/fldlg2/fldln2/fldz
+fldcw
+fldenv
+fmul/fmulp/fimul
+fcos/fsin/fsincos
+fsub/fsubp/fisub
+fsubr/fsubrp/fisubr
+int n/into/int 3
+ja|jae|jb|jbe|jc|jcxz|je|jecxz|jg|jge|jl|jle|jmp|jna|jnae|jnb|jnbe|jnc|jne|jng|jnge|jnl|jnle|jno|jnp|jns|jnz|jo|jp|jpe|jpo|js|jz
+lds/les/lfs/lgs/lss
+lgdt/lidt
+lldt
+loopz/loopnz ;intel documentation is funky about this form of LOOPcc
+
+
+
+stosb
+icebp
+int1
+smi
+
+;sse
+movdqu	xmm0, [vec]
+movhlps	xmm1, xmm0
+punpcklbw xmm0, xmm1
+movhlps	xmm1, xmm0
+punpcklbw xmm0, xmm1
+movdqu	[vec], xmm0
