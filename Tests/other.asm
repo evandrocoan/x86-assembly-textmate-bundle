9b863e003af2377e96a94cfd9d6f232e45df6742
13xforever <ilya.veselov@gmail.com>
2015-04-06 23:29:49 +0500

    + Tests for supplimentary syntax

---
+++ Tests/other.asm
@@ -0,0 +1,31 @@
+; one-line comment
+
+#if
+#elif
+#endif
+
+%if 0
+%endif
+
+#error \sdf\123
+#warning wer
+
+#define asdf = 123
+
+
+word dword qword tword yword
+db dw dd dq dt do dy
+res resb resw resd resq rest reso resy
+incbin equ times
+
+/* multiline
+   comment
+   block
+*/
+
+.data
+.bss
+.text
+
+db 'some string'
+db "another string\0"
