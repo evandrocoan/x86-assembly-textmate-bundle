a1f50f320c5eefffc40e05093da6ab622f9be059
13xforever <ilya.veselov@gmail.com>
2015-04-10 18:23:16 +0500

    Syntax highlight for new 512-bit registers

--- Tests/registers.asm
+++ Tests/registers.asm
@@ -8,6 +8,7 @@ r0d, r1d, r7d, r16d, r18d, r20d, r21d
 r0, r1, r7, r16, r18, r20, r21
 xmm, xmm01, xmm16, xmm20, xmm21
 ymm, ymm01, ymm16, ymm20, ymm21
+zmm, zmm01, zmm32, zmm40, zmm41
 dr4, dr5, dr8, dr9
 cr1, cr5, cr01, dtr
 st8, st9, st10, st11
@@ -49,6 +50,11 @@ xmm0, xmm1, xmm2, xmm3, xmm4, xmm5, xmm6, xmm7, xmm8, xmm9, xmm10, xmm11, xmm12,
 ;YMM registers (AVX)
 ymm0, ymm1, ymm2, ymm3, ymm4, ymm5, ymm6, ymm7, ymm8, ymm9, ymm10, ymm11, ymm12, ymm13, ymm14, ymm15
 
+;ZMM registers (AVX512)
+zmm0, zmm1, zmm2, zmm3, zmm4, zmm5, zmm6, zmm7, zmm8, zmm9,
+zmm10, zmm11, zmm12, zmm13, zmm14, zmm15, zmm16, zmm17, zmm18, zmm19,
+zmm20, zmm21, zmm22, zmm23, zmm24, zmm25, zmm26, zmm27, zmm28, zmm29,
+zmm30, zmm31
 ;control registers
 cr0, cr2, cr3, cr4
 
