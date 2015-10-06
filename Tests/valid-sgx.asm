733b8d5d279386386044cc0d724ddb7dfb3a396d
13xforever <ilya.veselov@gmail.com>
2015-10-06 13:31:01 +0500

    + added Intel SGX instructions

---
+++ Tests/valid-sgx.asm
@@ -0,0 +1,31 @@
+; Table 1-1.  Supervisor and User Mode Enclave Instruction Leaf Functions in Long-Form of SGX1
+;Supervisor Instruction
+eadd
+eblock
+ecreate
+edbgrd
+edbgwr
+eextend
+einit
+eldb
+eldu
+epa
+eremove
+etrack
+ewb
+;User Instruction
+eenter
+eexit
+egetkey
+ereport
+eresume
+
+;Table 1-2.  Supervisor and User Mode Enclave Instruction Leaf Functions in Long-Form of SGX2
+;Supervisor Instruction
+eaug
+emodpr
+emodt
+;User Instruction
+eaccept
+emodpe
+eacceptcopy
